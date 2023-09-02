8e1cec2b4c16ca89d97d45bce5e084f6f24cca4d
Minh Dao Nguyen <90809666+Minh-Dao-Nguyen@users.noreply.github.com>
2023-09-02 01:12:57 -0500

    Tailwind CSS installation

---
+++ Front End/Project-Manager-studio/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+
+      },
+      fontFamily: {
+
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px", 
+      md: "1060px", 
+    }
+  },
+  plugins: [],
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# Personal-Project-To-do-List
-
-Author: Minh Dao Nguyen and Hursh Jha
