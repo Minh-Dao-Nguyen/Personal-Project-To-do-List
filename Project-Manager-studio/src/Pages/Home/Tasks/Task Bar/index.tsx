3b5ad7fb13856a6bdd9cba151548c94a77ba8edc
Minh Dao Nguyen <90809666+Minh-Dao-Nguyen@users.noreply.github.com>
2024-01-29 23:27:36 -0600

    Create Task Bar

---
+++ Project-Manager-studio/src/Pages/Home/Tasks/Task Bar/index.tsx
@@ -0,0 +1,21 @@
+
+interface Props {
+    task: TaskData
+}
+
+const TaskBar = ( {task}: Props) => {
+    return (
+        <div className={'w-10/12 bg-gray-bg h-20 mb-5 flex-col justify-center'}>
+            <div className={'flex flex-row justify-between'}>
+                    <span className={'p-5 text-blue-300 text-xl flex-col justify-center'}>
+                        {task.name}
+                    </span>
+                <span className={'flex flex-col justify-center pr-10'}>
+                        status: {task.status}
+                    </span>
+            </div>
+        </div>
+    )
+}
+
+export default TaskBar;
