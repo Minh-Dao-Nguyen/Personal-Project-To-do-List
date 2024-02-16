53d2e2dd8f3904061f8bc916fbf69fafbbb52d20
Minh Dao Nguyen <90809666+Minh-Dao-Nguyen@users.noreply.github.com>
2024-02-16 15:33:01 -0600

    Fix overflow task bars

--- Project-Manager-studio/src/Pages/Home/Tasks/index.tsx
+++ Project-Manager-studio/src/Pages/Home/Tasks/index.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState, useRef} from "react";
 import TaskBar from "@/Pages/Home/Tasks/Task Bar";
 import {GetTaskData} from "@/Data/Service/MapData.ts";
 
@@ -14,15 +14,15 @@ const Tasks = () => {
         makeTaskList()
     }, []);
 
-
-
     return (
         <div className={`h-full w-full bg-primary-gray flex justify-center`}>
             <div className={'w-full h-full flex-col flex justify-center'}>
-                <div className={'w-full flex pl-20'}>
+                <div className={'w-full flex pl-20 mt-10 '}>
                     <button className={'border-2 p-5'}> Add new </button>
                 </div>
-                <div className={'flex flex-col justify-center w-full p-20'}>
+                <div className={'flex flex-col justify-center w-full p-20 overflow-scroll mt-10'}>
+                    <div className={'p-10'}>
+                    </div>
                     {taskList.map((item, index) => (
                         <TaskBar task={item} key={index} />
                     ))}
